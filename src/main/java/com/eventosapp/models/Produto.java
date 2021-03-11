2991bf8727d688de5810f9a6e3d859ba47589fb4
Luiz Miguel <luizmiguelandre332@gmail.com>
2021-03-11 19:26:20 -0300

    Ajuste no banco para ligar cliente e produto e melhora na praticidade de dar update nos dados do cliente.

--- src/main/java/com/eventosapp/models/Produto.java
+++ src/main/java/com/eventosapp/models/Produto.java
@@ -1,9 +1,11 @@
 package com.eventosapp.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 
 @Entity
 public class Produto {
@@ -11,17 +13,22 @@ public class Produto {
 	@Id
 	@GeneratedValue(strategy =
 			GenerationType.SEQUENCE, 
-	        generator = "id_produto")
-	private Long id_produto;
+	        generator = "id")
+	private Long id;
+	
+	@ManyToOne
+	@JoinColumn(name="idCliente")
+	private Cliente idCliente;
 	
 	private String nome_produto;
+	
 
-	public Long getId_produto() {
-		return id_produto;
+	public Long getId() {
+		return id;
 	}
 
-	public void setId_produto(Long id_produto) {
-		this.id_produto = id_produto;
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public String getNome_produto() {
